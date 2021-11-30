766d10e4c9bc310957b48dc67913faa6db5c86bf
Juan Soto <jsotcast@nttdata.com>
2021-11-30 11:22:44 -0500

    refactor add project

---
+++ product-service/src/main/java/com/nttdata/product/entity/ProductRule.java
@@ -0,0 +1,26 @@
+package com.nttdata.product.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductRule {
+    private boolean personal;
+    private boolean enterprise;
+
+    private boolean maintenanceCommission;
+    private boolean maximumMovementLimit;
+    private Integer movementAmount;
+    private Integer maximumPersonalAccount;
+    private Integer maximumEnterpriseAccount;
+    private Integer minimumHeadlines;
+    private Integer minimumAuthorizedSigners;
+
+    private Integer personalCreditAmount;
+    private Integer enterpriseCreditAmount;
+}
